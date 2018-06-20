5794ce8720a83f4d75991681664da7481444f4e1
T-107 <T-107@PC189.mshome.net>
2018-06-19 21:39:18 -0500

    Guardar

--- src/main/java/unitec/elementosmvc/ControladorMensaje.java
+++ src/main/java/unitec/elementosmvc/ControladorMensaje.java
@@ -1,12 +1,15 @@
 
 package unitec.elementosmvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +43,23 @@ public class ControladorMensaje {    //Clase controlador
         return repoMensa.findById(id).get();
     }
     
+    //Caso c) Guardar
+    @PostMapping("/mensaje")
+    public Estatus guardar(@RequestBody String json)throws Exception{
+        
+        //Primero convertimos este String json a un objeto Java
+        ObjectMapper maper=new ObjectMapper();
+        Mensaje mensa=maper.readValue(json, Mensaje.class);
+        repoMensa.save(mensa);
+        
+        System.out.println("Este objeto se convirtio: "+ mensa);
+        
+        Estatus estatus= new Estatus();
+        estatus.setSuccess(true);
+        estatus.setMensaje("Mensaje guardado con exito!!!");
+        
+        return estatus;
+        
+    }
     
 }
---
+++ src/main/java/unitec/elementosmvc/Estatus.java
@@ -0,0 +1,24 @@
+
+package unitec.elementosmvc;
+
+public class Estatus {
+    private boolean success;
+    private String mensaje;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
